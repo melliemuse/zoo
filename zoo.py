fcba1f87cbd103e06e87ec406e16c1b57863f665
Melody Stern <melliemuse@gmail.com>
2020-01-13 21:46:06 -0600

    setup file

---
+++ zoo.py
@@ -0,0 +1,20 @@
+# Create a tuple named zoo that contains 10 of your favorite animals.
+# Find one of your animals using the tuple.index(value) syntax on the tuple.
+# Example
+flowers = ("daisy", "rose")
+print(flowers.index("rose")) # Output is 1
+# Determine if an animal is in your tuple by using value in tuple syntax.
+animal_to_find = "kangaroo"
+# if animal_to_find in zoo:
+    # Print that the animal was found
+# You can reverse engineer (unpack) a tuple into another tuple with the following syntax.
+children = ("Sally", "Hansel", "Gretel", "Svetlana")
+(first_child, second_child, third_child, fourth_child) = children
+print(first_child) # Output is "Sally"
+print(second_child) # Output is "Hansel"
+print(third_child) # Output is "Gretel"
+print(fourth_child) # Output is "Svetlana"
+# Create a variable for the animals in your zoo tuple, and print them to the console.
+# Convert your tuple into a list.
+# Use extend() to add three more animals to your zoo.
+# Convert the list back into a tuple. 
